e482f91e92582764eb3e4e2a4defc983a5d44958
HectorBussan <hector141599@hotmail.com>
2023-01-01 09:56:03 -0600

    Agregando función de json para leer los requests

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import { PORT } from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 const app = express();
-
+app.use(express.json());
 app.use(indexRoutes);
 app.use(taskRoutes);
 
