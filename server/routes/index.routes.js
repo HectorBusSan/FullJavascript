b2baa5ec01f665e14699b69cc08ef2fe59d156c6
HectorBussan <hector141599@hotmail.com>
2023-01-01 09:28:39 -0600

    Agregando route a consulta con ping y mysql

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -2,8 +2,10 @@ import { Router } from "express";
 import { pool } from "../db.js";
 const router = Router();
 
-router.get("/ping",(req,res)=>{
-
+router.get("/ping",async(req,res)=>{
+    const result = await pool.query("Select 1 + 1 as result");
+    console.log(result)
+    res.json("ping");
 })
 
 export default router;
