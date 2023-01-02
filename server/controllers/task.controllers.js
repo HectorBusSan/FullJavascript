7f16ff6735010751d1f76521176582707783e175
HectorBussan <hector141599@hotmail.com>
2023-01-02 11:36:43 -0600

    Creando solicitudes get a la base de datos

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -1,10 +1,14 @@
 import { pool } from "../db.js";
 
-export const getTasks=(req,res)=>{
-    res.send("Obteniendo tareas");
+export const getTasks=async(req,res)=>{
+    const [result]=await pool.query("Select * from task order by createdAt");
+    res.json(result)
 }
-export const getTask=(req,res)=>{
-    res.send("Obteniendo tarea");
+export const getTask=async(req,res)=>{
+    const id=req.params.id;
+    const [result] = await pool.query("Select * from task where id=?",id);
+    console.log(result);
+    res.json(result);
 }
 export const createTask=async(req,res)=>{
     const {title,description} = req.body;
