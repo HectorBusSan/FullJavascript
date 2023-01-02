7a9616cefc8534821ece3e27d0d08b5c0eabeb7a
HectorBussan <hector141599@hotmail.com>
2023-01-02 11:05:44 -0600

    Agregando insert a createTask

--- server/controllers/task.controllers.js
+++ server/controllers/task.controllers.js
@@ -1,12 +1,16 @@
+import { pool } from "../db.js";
+
 export const getTasks=(req,res)=>{
     res.send("Obteniendo tareas");
 }
 export const getTask=(req,res)=>{
     res.send("Obteniendo tarea");
 }
-export const createTask=(req,res)=>{
-    console.log(req.body);
-    res.send("creando...")
+export const createTask=async(req,res)=>{
+    const {title,description} = req.body;
+    const result = await pool.query("Insert into task(title,description) values (?,?)",[title,description])
+    console.log(result)
+    res.send("creado tarea")
 }
 export const updateTask=(req,res)=>{
     res.send("Actualizando tarea");
