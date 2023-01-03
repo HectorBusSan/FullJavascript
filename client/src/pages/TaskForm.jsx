62db3de342b761a979e03d016d3ccf2e9cbfc047
HectorBussan <hector141599@hotmail.com>
2023-01-03 16:03:35 -0600

    Creando Form con Formik

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import {Formik} from "formik"
 function TaskForm() {
   return (
-    <div>TaskForm</div>
+    <div>
+        <Formk>
+
+        </Formk>
+    </div>
   )
 }
 
