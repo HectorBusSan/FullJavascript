d5d5341a66ebde06167fb4d4568422acdf4d6a96
HectorBussan <hector141599@hotmail.com>
2023-01-03 16:09:06 -0600

    Agregando rutas a App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,17 @@ import { Route, Routes } from "react-router-dom"
 import NotFound from "./pages/NotFound"
 import TaskForm from "./pages/TaskForm"
 import TaskPages from "./pages/TaskPages"
-
+import Navigation from "./components/Navigation"
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<TaskPages/>}/>
-      <Route path="/new" element={<TaskForm/>}/>
-      <Route path="*" element={<NotFound/>}/>
-    </Routes>
+    <>
+    <Navigation/>
+      <Routes>
+        <Route path="/" element={<TaskPages/>}/>
+        <Route path="/new" element={<TaskForm/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
+    </>
   )
 }
 
