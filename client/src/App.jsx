ce55e0761f089f0e1b89c5506c900f1551b72ef4
HectorBussan <hector141599@hotmail.com>
2023-01-03 15:13:54 -0600

    Agregando las distantas routas con path y su elementos correspodiente

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import NotFound from "./pages/NotFound"
+import TaskForm from "./pages/TaskForm"
+import TaskPages from "./pages/TaskPages"
+
 function App() {
   return (
-    <div>Hello World</div>
+    <Routes>
+      <Route path="/" element={<TaskPages/>}/>
+      <Route path="/new" element={<TaskForm/>}/>
+      <Route path="*" element={<NotFound/>}/>
+    </Routes>
   )
 }
 
