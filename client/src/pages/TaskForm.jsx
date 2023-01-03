2280cdd98808693e1817799b64647f7b1bfac268
HectorBussan <hector141599@hotmail.com>
2023-01-03 16:15:10 -0600

    Agregando Form

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import {Fomr,Formik} from "formik"
+import {Form,Formik} from "formik"
 function TaskForm() {
   return (
     <div>
         <Formik>
             <Form>
+                <label>Title</label>
+                <input type="text" name="title" placeholder='Title...'/>
 
+                <label>Description</label>
+                <textarea type="text" name="description" placeholder='Write a description...'></textarea>
+                <button>Save</button>
             </Form>
         </Formik>
     </div>
