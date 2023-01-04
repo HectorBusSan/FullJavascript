3e8081f03fc2c6badf02eca69b5e8498ab4bb163
HectorBussan <hector141599@hotmail.com>
2023-01-04 12:12:49 -0600

    Importando y Agregando TaskContextProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,18 @@ import { Route, Routes } from "react-router-dom"
 import NotFound from "./pages/NotFound"
 import TaskForm from "./pages/TaskForm"
 import TaskPages from "./pages/TaskPages"
+import { TaskContextProvider } from "./context/taskContext"
 import Navigation from "./components/Navigation"
 function App() {
   return (
-    <>
+    <TaskContextProvider>
     <Navigation/>
       <Routes>
         <Route path="/" element={<TaskPages/>}/>
         <Route path="/new" element={<TaskForm/>}/>
         <Route path="*" element={<NotFound/>}/>
       </Routes>
-    </>
+    </TaskContextProvider>
   )
 }
 
