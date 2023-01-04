a8292f06e3b58a224814fa09a6d827d29be86aec
HectorBussan <hector141599@hotmail.com>
2023-01-04 11:45:11 -0600

    Creando componente decarta para las {task}

---
+++ client/src/components/TaskCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TaskCard({task}) {
+  return (
+    <div key={task.id}>
+        <h2>{task.title}</h2>
+        <p>{task.description}</p>
+        <span>{task.done===1?"✔️":"❌"}</span>
+        <span>{task.createdAt}</span>
+        <button>Delete</button>
+        <button>Edit</button>
+    </div>
+  )
+}
+
+export default TaskCard
