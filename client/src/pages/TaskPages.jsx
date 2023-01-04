5467f41ab49a47610166b9159af261e7ecefb305
HectorBussan <hector141599@hotmail.com>
2023-01-04 11:42:47 -0600

    Agregando Tareas a la Interfaz

--- client/src/pages/TaskPages.jsx
+++ client/src/pages/TaskPages.jsx
@@ -2,11 +2,12 @@ import { useEffect,useState } from 'react'
 import { getTasksRequest } from '../api/task.api.js'
 function TaskPages() {
 
-  const [task, setTask] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   useEffect(() => {  
     async function LoadTasks(){
       const response= await getTasksRequest();
+      setTasks(response);
       console.log(response);
     }
     LoadTasks();
@@ -16,7 +17,18 @@ function TaskPages() {
   return (
     <div>
       <h1>Task</h1>
-
+      {
+        tasks.map(task=>(
+          <div key={task.id}>
+            <h2>{task.title}</h2>
+            <p>{task.description}</p>
+            <span>{task.done===1?"✔️":"❌"}</span>
+            <span>{task.createdAt}</span>
+            <button>Delete</button>
+            <button>Edit</button>
+          </div>
+        ))
+      }
     </div>
   )
 }
