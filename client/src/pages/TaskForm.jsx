be3faf0e670d4affe35ea1eb491a2a6f75a39c6d
HectorBussan <hector141599@hotmail.com>
2023-01-04 10:56:39 -0600

    Enviando los datos al servidor

--- client/src/pages/TaskForm.jsx
+++ client/src/pages/TaskForm.jsx
@@ -9,24 +9,29 @@ function TaskForm() {
             title:"",
             description:""
         }}
-        onSubmit={async(values)=>{
+        onSubmit={async(values,actions)=>{
             console.log(values);
             try{
                 const response= await createTaskRequest(values);
                 console.log(response);
+                actions.resetForm();
             }catch(err){
                 console.log(err);
             }
         }}>
             {
-                ({handleChange, handleSubmit})=>(
+                ({handleChange, handleSubmit, values, isSubmitting})=>(
                     <Form on onSubmit={handleSubmit}>
                         <label>Title</label>
-                        <input type="text" name="title" placeholder='Title...' onChange={handleChange}/>
+                        <input type="text" name="title" placeholder='Title...' onChange={handleChange}
+                        value={values.title}/>
         
                         <label>Description</label>
-                        <textarea type="text" name="description" placeholder='Write a description...' onChange={handleChange}></textarea>
-                        <button type='submit'>Save</button>
+                        <textarea type="text" name="description" placeholder='Write a description...' onChange={handleChange}
+                        value={values.description}></textarea>
+                        <button type='submit' disabled={isSubmitting}>
+                            {isSubmitting?"Saving...":"Save"}
+                        </button>
                     </Form>       
                 )
             }
