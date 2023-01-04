79c22987468746ce01e2992a70796c050e045b7b
HectorBussan <hector141599@hotmail.com>
2023-01-04 10:26:18 -0600

    Agregando cors para conectarse al servidor

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from "express"
+import cors from "cors"
 import { PORT } from "./config.js";
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 const app = express();
+app.use(cors({
+    origin: "https://localhost:5173"
+}));
 app.use(express.json());
 app.use(indexRoutes);
 app.use(taskRoutes);
