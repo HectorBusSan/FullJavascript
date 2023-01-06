22785e73b1a64da7e1fbeaa4ff2fb220713fe9d8
HectorBussan <hector141599@hotmail.com>
2023-01-06 10:25:41 -0600

    Todo useTask debe estar dentro del TaskCOntextProvider para ser usado

--- client/src/context/taskContext.jsx
+++ client/src/context/taskContext.jsx
@@ -1,9 +1,11 @@
-import { createContext } from "react";
+import { createContext,useContext } from "react";
 
 export const TaskContext= createContext();
 
 export const  useTasks=()=>{
-    
+    const context=useContext(TaskContext)
+    if(!context) throw new Error("useTask must be used within a TaskContextProvider");
+    return context;
 }
 
 export const TaskContextProvider=({children})=>{
