2968b374514c6ba3326a22628095239429b32326
HectorBussan <hector141599@hotmail.com>
2023-01-07 08:12:32 -0600

    Agregando formato a la barra de navegación

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function Navigation() {
   return (
-    <div className='bg-zinc-700 flex justify-between px-10'>
-        <h1>React MySQL</h1>
-        <ul className='flex'>
+    <div className='bg-zinc-700 flex justify-between px-20 py-2'>
+        <Link to="/" className='text-white font-bold text-lg'>
+          <h1>React MySQL</h1>
+        </Link>
+        <ul className='flex gap-x-2'>
             <li>
-                <Link to="/">Home</Link>
+                <Link className='bg-gray-100 px-2 py-1 rounded' to="/">Home</Link>
             </li>
             <li>
-                <Link to="/new">Create task</Link>
+                <Link className='bg-gray-100 px-2 py-1 rounded' to="/new">Create task</Link>
             </li>
         </ul>
     </div>
