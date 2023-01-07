c64b3b81670215c5ded84c0b78f5c57687ce7535
HectorBussan <hector141599@hotmail.com>
2023-01-07 05:53:12 -0600

    Agregando botton para cambiar si la terea esta hecha o no

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -4,12 +4,16 @@ import { useNavigate } from 'react-router-dom';
 function TaskCard({task}) {
   const {deleteTask}=useTasks();
   const navigate= useNavigate();
-
+  const handlerDone=(taskDone)=>{
+    if(taskDone===true) {}
+  }
   return (
     <div>
         <h2>{task.title}</h2>
         <p>{task.description}</p>
-        <span>{task.done===1?"✔️":"❌"}</span>
+        <button onClick={()=>{handlerDone(task.done)}}>
+          <span>{task.done===1?"✔️":"❌"}</span>
+        </button>
         <span>{task.createdAt}</span>
         <button onClick={()=>{deleteTask(task.id)}}>Delete</button>
         <button onClick={()=>{navigate(`/edit/${task.id}`)}}>Edit</button>
