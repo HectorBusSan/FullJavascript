7da79fcaf9e9a28d15a37de0832fb3b1358a6299
HectorBussan <hector141599@hotmail.com>
2023-01-07 08:02:48 -0600

    Reubicando boton de check

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -11,13 +11,13 @@ function TaskCard({task}) {
     <div className='bg-zinc-700 p-4 rounded text-white shadow-lg hover:shadow-cyan-500/50 ease-linear duration-300'>
         <header className='flex justify-between uppercase'>
           <h2 className='text-sm font-bold'>{task.title}</h2>
+          <button className='bg-gray-200 px-2 py-1 text-white rounded m-1 hover:bg-sky-700 ease-in duration-300' onClick={()=>{handlerDone()}}>
+            <span>{task.done===1?"✔️":"❌"}</span>
+          </button>
         </header>
         <p className='text-xs'>{task.description}</p>
-        <button className='bg-gray-200 px-2 py-1 text-white rounded m-1 hover:bg-sky-700 ease-in duration-300' onClick={()=>{handlerDone()}}>
-          <span>{task.done===1?"✔️":"❌"}</span>
-        </button>
         <span>{task.createdAt}</span>
-        <div className=' flex gap-x-2'>
+        <div className=' flex gap-x-3'>
           <button className='bg-red-500 px-2 py-1 text-white rounded' onClick={()=>{deleteTask(task.id)}}>Delete</button>
           <button className='bg-blue-500 px-2 py-1 text-white rounded' onClick={()=>{navigate(`/edit/${task.id}`)}}>Edit</button>
         </div>
