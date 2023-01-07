3a5d92dd828e12273c4150d0d3642e5988eab081
HectorBussan <hector141599@hotmail.com>
2023-01-07 08:47:45 -0600

    Agregando reguimiento de la barra de navegación

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 function Navigation() {
   return (
-    <div className='bg-zinc-700 flex justify-between px-20 py-2'>
+    <div className='bg-zinc-700 flex justify-between px-20 py-2 fixed w-full'>
         <Link to="/" className='text-white font-bold text-lg'>
           <h1>React MySQL</h1>
         </Link>
