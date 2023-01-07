f7fea5267045a24b8a88f73b14e1cefd1a29beaf
HectorBussan <hector141599@hotmail.com>
2023-01-07 07:56:34 -0600

    Agregando hover y transition a las tarjetas

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -8,18 +8,18 @@ function TaskCard({task}) {
     await toggleTaskDone(task.id)
   }
   return (
-    <div className='bg-slate-300 p-4 rounded'>
-        <header className='flex justify-between'>
+    <div className='bg-zinc-700 p-4 rounded text-white shadow-lg hover:shadow-cyan-500/50 ease-linear duration-300'>
+        <header className='flex justify-between uppercase'>
           <h2 className='text-sm font-bold'>{task.title}</h2>
         </header>
-        <p>{task.description}</p>
-        <button onClick={()=>{handlerDone()}}>
+        <p className='text-xs'>{task.description}</p>
+        <button className='bg-gray-200 px-2 py-1 text-white rounded m-1 hover:bg-sky-700 ease-in duration-300' onClick={()=>{handlerDone()}}>
           <span>{task.done===1?"✔️":"❌"}</span>
         </button>
         <span>{task.createdAt}</span>
-        <div>
-          <button onClick={()=>{deleteTask(task.id)}}>Delete</button>
-          <button onClick={()=>{navigate(`/edit/${task.id}`)}}>Edit</button>
+        <div className=' flex gap-x-2'>
+          <button className='bg-red-500 px-2 py-1 text-white rounded' onClick={()=>{deleteTask(task.id)}}>Delete</button>
+          <button className='bg-blue-500 px-2 py-1 text-white rounded' onClick={()=>{navigate(`/edit/${task.id}`)}}>Edit</button>
         </div>
     </div>
   )
