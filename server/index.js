7f22f44e609b7b0beebfeb78886fce78568edd1e
HectorBussan <hector141599@hotmail.com>
2023-01-08 07:16:50 -0600

    Despues de crear build con npm run build creamos la ruta especifica

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 import express from "express"
 import cors from "cors"
 import { PORT } from "./config.js";
+import {dirname} from "path";
+import {fileURLToPath } from "url";
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 const app = express();
+const __dirname=
 
 app.use(cors());
 
 app.use(express.json());
 app.use(indexRoutes);
 app.use(taskRoutes);
+app.use(express.static())
 
 app.listen(PORT)
 console.log(`Server is running on port ${PORT}`);
