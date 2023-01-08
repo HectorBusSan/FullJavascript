be7f8fedfeedf6888dc3bdb1fee8e26d5493d9f4
HectorBussan <hector141599@hotmail.com>
2023-01-08 07:19:32 -0600

    Con __dirname podremos encontrar la ruta exacta de donde se ejecuta el programa

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ import {fileURLToPath } from "url";
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/task.routes.js";
 const app = express();
-const __dirname=
-
+const __dirname=dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 app.use(cors());
 
 app.use(express.json());
 app.use(indexRoutes);
 app.use(taskRoutes);
-app.use(express.static())
+// app.use(express.static())
 
 app.listen(PORT)
 console.log(`Server is running on port ${PORT}`);
